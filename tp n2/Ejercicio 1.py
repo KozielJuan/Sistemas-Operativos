a2c35baea3f001f3229111996155964a29539962
ThiagoLoBalbo <168198864+KozielJuan@users.noreply.github.com>
2025-05-27 10:35:24 -0300

    mai

--- tp n2/Ejercicio 1.py
+++ tp n2/Ejercicio 1.py
@@ -1,3 +1,8 @@
-radio=int(input("Ingrese el radio: "))
-v = (4/3)*3.1416*radio**3
-print("El volumen de la esfera es: ",v)
+cuenta = float(input("Ingrese el total de la cuenta: $"))
+porcentaje = float(input("Ingrese el porcentaje de propina que desea dejar: "))
+
+propina = cuenta * (porcentaje / 100)
+total = cuenta + propina
+
+print(f"Propina: ${propina:.2f}")
+print(f"Total a pagar: ${total:.2f}")
